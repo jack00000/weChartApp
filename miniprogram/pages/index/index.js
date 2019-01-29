0f21c13ec585a3b7c9733449025973547a893031
unknown <unknown@example.com>
2019-01-29 12:54:24 +0800

    调用corlorUI成功。
    corlorUi 有个tempert文件  是一个引用啦corlorUi的工程。vantUI iviewUI 都有使用文档。corlorUi没文档，直接在项目里拷代码就行。

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,7 +7,67 @@ Page({
     userInfo: {},
     logged: false,
     takeSession: false,
-    requestResult: ''
+    requestResult: '',
+    active: 0,
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    iconList: [{
+      icon: 'cardboardfill',
+      color: 'red',
+      badge: 120,
+      name: 'VR'
+    }, {
+      icon: 'recordfill',
+      color: 'orange',
+      badge: 1,
+      name: '录像'
+    }, {
+      icon: 'picfill',
+      color: 'yellow',
+      badge: 0,
+      name: '图像'
+    }, {
+      icon: 'noticefill',
+      color: 'olive',
+      badge: 22,
+      name: '通知'
+    }, {
+      icon: 'upstagefill',
+      color: 'cyan',
+      badge: 0,
+      name: '排行榜'
+    }, {
+      icon: 'clothesfill',
+      color: 'blue',
+      badge: 0,
+      name: '皮肤'
+    }, {
+      icon: 'discoverfill',
+      color: 'purple',
+      badge: 0,
+      name: '发现'
+    }, {
+      icon: 'questionfill',
+      color: 'mauve',
+      badge: 0,
+      name: '帮助'
+    }, {
+      icon: 'commandfill',
+      color: 'purple',
+      badge: 0,
+      name: '问答'
+    }, {
+      icon: 'brandfill',
+      color: 'mauve',
+      badge: 0,
+      name: '版权'
+    }],
+    gridCol: 3,
+    skin: false
+  },
+  // event.detail 的值为当前选中项的索引
+  onChange(event) {
+    console.log(event.detail);
   },
   onLoad: function (options) {
     wx.request({
@@ -20,6 +80,7 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       success(res) {
+        debugger;
         console.log(res.data)
       }
     })
@@ -38,7 +99,77 @@ Page({
         console.log(res.data)
       }
     })
-  }
+  },
+  showModal(e) {
+    this.setData({
+      modalName: e.currentTarget.dataset.target
+    })
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
+  },
+  gridchange: function (e) {
+    this.setData({
+      gridCol: e.detail.value
+    });
+  },
+  gridswitch: function (e) {
+    this.setData({
+      gridBorder: e.detail.value
+    });
+  },
+  menuBorder: function (e) {
+    this.setData({
+      menuBorder: e.detail.value
+    });
+  },
+  menuArrow: function (e) {
+    this.setData({
+      menuArrow: e.detail.value
+    });
+  },
+  menuCard: function (e) {
+    this.setData({
+      menuCard: e.detail.value
+    });
+  },
+  switchSex: function (e) {
+    this.setData({
+      skin: e.detail.value
+    });
+  },
+
+  // ListTouch触摸开始
+  ListTouchStart(e) {
+    this.setData({
+      ListTouchStart: e.touches[0].pageX
+    })
+  },
+
+  // ListTouch计算方向
+  ListTouchMove(e) {
+    this.setData({
+      ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
+    })
+  },
+
+  // ListTouch计算滚动
+  ListTouchEnd(e) {
+    if (this.data.ListTouchDirection == 'left') {
+      this.setData({
+        modalName: e.currentTarget.dataset.target
+      })
+    } else {
+      this.setData({
+        modalName: null
+      })
+    }
+    this.setData({
+      ListTouchDirection: null
+    })
+  },
   
   
 })
