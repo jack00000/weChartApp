1e0629e33e99d895ce401d03bfe32caa82d01524
demo <1558185399@example.com>
2019-02-01 16:10:38 +0800

    下拉刷新初步完成
    样式功能待调

--- miniprogram/pages/new/new.js
+++ miniprogram/pages/new/new.js
@@ -206,6 +206,19 @@ Page({
     wx.redirectTo({
       url: '../my/my',
     })
+  },
+  //下拉刷新
+  onPullDownRefresh: function () {
+    wx.request({
+      url: 'http://127.0.0.1:8080/ssm/getCategory',
+      data: {},
+      method: 'GET',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) {
+        wx.stopPullDownRefresh();
+      }
+    })
   }
 
 })
--- miniprogram/pages/news/chat/chat.js
+++ miniprogram/pages/news/chat/chat.js
@@ -5,5 +5,18 @@ Page({
     CustomBar: app.globalData.CustomBar
   },
   onLoad: function () { },
+  //下拉刷新
+  onPullDownRefresh: function () {
+    wx.request({
+      url: 'http://127.0.0.1:8080/ssm/getCategory',
+      data: {},
+      method: 'GET',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) {
+        wx.stopPullDownRefresh();
+      }
+    })
+  }
 
 });
