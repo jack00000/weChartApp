9963277664b7cdbad244bd207a4a8684299aca96
fangjun <fangjun@esensoft.com>
2019-04-15 11:39:18 +0800

    增加图灵api(公司电脑)

--- miniprogram/pages/news/chat/index.js
+++ miniprogram/pages/news/chat/index.js
@@ -192,5 +192,31 @@ Page({
     wx.redirectTo({
       url: '../../news/news',
     })
-  }
+  },
+  //图灵机器人api
+  converSation: function (e) {
+    let that = this
+    var obj = {},
+      isay = e.detail.value.says,
+      syas = that.data.syas,
+      length = syas.length,
+      key = 'apikey'//这里填入你得到的图灵机器人的apikey
+
+    console.log(length)
+    wx.request({
+      url: 'http://www.tuling123.com/openapi/api?key=' + key + '&info=' + isay,
+      success: function (res) {
+        let tuling = res.data.text;
+        obj.robot = tuling;
+        obj.isay = isay;
+        syas[length] = obj;
+        that.setData({
+          syas: syas
+        })
+      }
+    })
+
+
+  },
+
 })
